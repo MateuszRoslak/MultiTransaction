46c32006cc5c5bf53cc1ed63c3827b590df67b7a
Artur Jaworski <artur.jaworski@htdevelopers.com>
2022-06-06 13:16:00 +0200

    refactor

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -7,8 +7,7 @@ class CheckoutsController < ApplicationController
     Payments::StripePayment::CreateSession.call(user: current_user) do |on|
       on.success { |session| redirect_to session.url, status: 303, allow_other_host: true }
       on.failure do |error|
-        flash[:alert] = error
-        redirect_to cart_path
+        redirect_to cart_path, alert: error
       end
     end
   end
@@ -17,7 +16,6 @@ class CheckoutsController < ApplicationController
     # TODO: After creating the transaction history model, add the purchase summary screen
     current_user.line_items.destroy_all
 
-    flash[:notice] = 'Purchase was successful!'
-    redirect_to cart_path
+    redirect_to cart_path, notice: 'Purchase was successful!'
   end
 end
