aad6e06c3c28f8b20b469629fcc462ee8cbb57ad
Artur Jaworski <artur.jaworski@htdevelopers.com>
2022-06-15 13:10:25 +0200

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
 
   devise_scope :user do
-    # Redirests signing out users back to sign-in
+    # Redirects signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
